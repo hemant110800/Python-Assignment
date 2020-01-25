de37d5a9fb634024b0488fb48ee4477eaeb00fed
Hemant Bhati <hemant110800@gmail.com>
2020-01-26 04:42:53 +0530

    Delete name_and_marks.py

--- Module3/name_and_marks.py
+++
@@ -1,5 +0,0 @@
-res=input()
-l1=res.split(",")
-l2=[int(i) for i in l1[1:]]
-print(l1[0],"obtained",sum(l2[0:-3]),"out of","500 in theory and",sum(l2[-3:]),"""marks out of 90 in practical and
-successfully passed the exam with""","{0:.2f}".format((sum(l2[0:-3])+sum(l2[-3:]))/590*100),"% in aggregate.Many congrulations to",l1[0],".")
