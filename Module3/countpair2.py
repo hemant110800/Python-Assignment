2e9258db86896fec82626734def3aed1eb04bbfa
Hemant Bhati <hemant110800@gmail.com>
2020-01-26 04:42:18 +0530

    Delete countpair2.py

--- Module3/countpair2.py
+++
@@ -1,9 +0,0 @@
-sum1=int(input("enter sum you want"))
-n1=list(map(int,input("Enter list1").split()))
-n2=list(map(int,input("Enter list2").split()))
-l1=[]
-for i in n1:
-    if(sum1-i) in n2:
-        l1.append([i,sum1-i])
-for i in l1:
-    print("Possible Pair-",i)
